da970e274c72c9e9d77ef73c716af5ea2752a25e
Emanuele2025 <emanuelemattei@tutanota.com>
2026-07-28 17:49:22 +0200

    Pacchetto setup

--- Lupo/obj/Debug/net10.0-windows/Lupo.AssemblyInfo.cs
+++ Lupo/obj/Debug/net10.0-windows/Lupo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lupo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+877cc6ab543147bc7b05d6348aec3ac93d13ce0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2aa08f25092a4c2716896733268cadd6a72294")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lupo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lupo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
